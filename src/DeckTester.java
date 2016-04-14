9915bcfadc43184f6afe12698ceac8844fd180f8
Joshua Madden <Joshua Madden>
2016-04-13 18:47:08 -0600

    finished a2

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -8,6 +8,25 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks1 = {"2", "4", "6", "King"};
+		String[] suits1 = {"Hearts", "Clubs", "Spades", "Diamonds"};
+		int[] values1 = {2, 4, 6, 13};
+	    Deck deck1 = new Deck(ranks1, suits1, values1);
+	    System.out.println("How many cards are in this deck? " + deck1.size());
+	   
+	    
+	    String[] ranks2 = {};
+	    String[] suits2 = {};
+	    int[] values2 = {};
+	    Deck deck2 = new Deck(ranks2, suits2, values2);
+	    System.out.println("This deck should be empty, is it? " + deck2.isEmpty());
+	    
+	    String[] ranks3 = {"1", "3", "Queen", "2", "4", "6", "King"};
+	    String[] suits3 = {"Diamonds", "Spades", "Clubs", "Spades", "Diamonds", "Spades", "Hearts"};
+	    int[] values3 = {1, 3, 12, 2, 4, 6, 13};
+	    Deck deck3 = new Deck(ranks3, suits3, values3);
+	    System.out.println("If i deal a card from this deck i will get is the: " + deck3.deal());
+	    System.out.println("If i deal a card from this deck i will get is the: " + deck3.deal());
+	    System.out.println("If i deal a card from this deck i will get is the: " + deck3.deal());
 	}
 }
